63ad5cfe2e37fca619d69dc988965f8f6742a765
kartik <kartik@192.168.0.101>
2017-04-09 12:48:33 +0530

    Initial Commit of java 8 tutorials.

---
+++ Java8lambdaTutorials/src/com/AppMain.java
@@ -0,0 +1,17 @@
+package com;
+
+/**
+ * <p>
+ * Main Entry Point Of Tutorial.
+ * </p>
+ * 
+ * @author kartik
+ *
+ */
+public class AppMain {
+
+	public static void main(String[] args) {
+
+	}	
+
+}
